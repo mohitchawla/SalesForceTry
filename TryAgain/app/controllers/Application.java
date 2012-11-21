f0c833e720591364af0d38289f02197636438b17
mohitchawla <mohitchawlacool@gmail.com>
2012-11-21 17:20:29 +0530

    Happy

---
+++ TryAgain/app/controllers/Application.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+    	System.out.println("mohit Chawla 2");
+    	System.out.println("mohit Chawla 1");
+    	System.out.println("mohit Chawla 3");
+        render();
+    }
+
+}
